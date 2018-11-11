e28bda59582f1166fe40eecaa56be161da4ddaf7
Thomas Lane <thomas_lane@icloud.com>
2018-11-11 13:30:24 -0800

    Update tarbs.sh

--- tarbs.sh
+++ tarbs.sh
@@ -203,9 +203,10 @@ manualinstall $aurhelper
 installationloop
 
 
-
+mkdir tester
 #setup zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$name/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git tester/.oh-my-zsh
+cp -r tester/.oh-my-zsh /home/$name/.oh-my-zsh
 usermod --shell /bin/zsh $name
 
 #extra fonts needed
@@ -218,7 +219,7 @@ rm -rf fonts
 #add repos
 putgitrepo "$dotfilesrepo" "/home/$name"
 putgitrepo "$dotfilesrepo2" "/home/$name"
-putgitrepo "$rootfilesrepo" "/"
+#putgitrepo "$rootfilesrepo" "/"
 
 
 # Install the TARBS Firefox profile in ~/.mozilla/firefox/
